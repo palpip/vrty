bde2a263b2530980b05d3c96ec5db6d0b699266d
pp <pp@envigeo.sk>
2026-06-29 08:48:24 -0700

    upravene bat

--- stepsNopause.bat
+++ stepsNopause.bat
@@ -2,21 +2,21 @@
 .\.venv\scripts\python Krok1Prepare.py
 @echo "spustenie krokov 1 - 5 - teda až po spojenie a vygenerovanie spojeného xls a kml"
 @echo "exportovan0 údaje zo súborov *vrt a vrl v dávke"
-@pause
+rem @pause
 
 
 .\.venv\scripts\python Krok2ReadGeo5.py
 .\.venv\scripts\python Krok2readvrtcsvQGIS.py
 .\.venv\scripts\python Krok2ReadVrtLozCSVQGIS.py
 @echo "vytvorené 3 súbory csv z dávky pre jednotlivé typy vrtov, umiestnené v TOPDIR"
-@pause
+rem @pause
 
 .\.venv\scripts\python Krok3importcsvPandas.py
 @echo "spojené 3 csv z dávky do jediného xls, umiestnené v TOPDIR a jeho kópia v joindir"
-@pause
+rem @pause
 .\.venv\scripts\python Krok4CreateKML.py
 @echo "zo spojeného xls vytvorené kml dávky v TOPDIR"
-@pause
+rem @pause
 .\.venv\scripts\python Krok5Joinxls.py
 @echo "zo xlsx súborov dávok v joindir vytvorené spojené xlsx a spojené kml"
 @echo "v pripravte pdf súbory v http serveri a spustite test - krok6"
